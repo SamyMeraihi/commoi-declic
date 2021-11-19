54914ef01b94d6d7158fa7c65ada26f8767a7e25
Samy MERAIHI <samy.meraihi@gmail.com>
2021-11-20 00:21:32 +0100

    ajout du watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function buildStyles() {
-  return gulp.src('./scss/main.scss')
-    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./scss/main.scss', ['sass']);
-};
+// Watcher qui surveille les modifications des fichiers dans le dossier /scss
+gulp.task('watch', function () {
+  gulp.watch('./scss/main.scss', gulp.parallel('buildStyles'));
+})
+
+// Tâche appelée par le Watcher qui compile le scss en css
+gulp.task('buildStyles', function () {
+  gulp.src('./scss/main.scss')
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+})
