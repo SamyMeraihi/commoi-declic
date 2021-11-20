97dc67780319478fd6d70ea83e03c57051d01b5e
Allaoui <sa_allaoui@bitbucket.org>
2021-11-20 11:19:28 +0100

    update du watcher dans gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
+// Tâche appelée par le Watcher qui compile le scss en css
+gulp.task('buildStyles', function () {
+  return gulp.src('./scss/main.scss')
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+})
 
 // Watcher qui surveille les modifications des fichiers dans le dossier /scss
 gulp.task('watch', function () {
   gulp.watch('./scss/main.scss', gulp.parallel('buildStyles'));
-})
-
-// Tâche appelée par le Watcher qui compile le scss en css
-gulp.task('buildStyles', function () {
-  gulp.src('./scss/main.scss')
-    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
 })
