dfc482e7854628e270b6752898576baf1bb62f55
Allaoui <sa_allaoui@bitbucket.org>
2021-12-03 13:59:32 +0100

    css responsive + animation burger + images flottantes

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@ let burger = document.querySelector(".burger-off");
 burger.onclick = function() {
   this.classList.toggle("burger-off");
   this.classList.toggle("burger-on");
+  document.querySelector(".dropdown").classList.toggle("dropdown-on");
 }
