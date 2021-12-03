ff94eb676c0682ef90a801c5656e0845983e58d6
Allaoui <sa_allaoui@bitbucket.org>
2021-12-03 05:22:42 +0100

    css responsive

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Animation menu burger
+let burger = document.querySelector(".burger-off");
+
+burger.onclick = function() {
+  this.classList.toggle("burger-off");
+  this.classList.toggle("burger-on");
+}
