22c00db722756d418d0cb1c1c5f86faa7fe55dd1
Samy MERAIHI <samy.meraihi@gmail.com>
2021-12-04 14:07:47 +0100

    animations boutons

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //Animation menu burger
 let burger = document.querySelector(".burger-off");
 
-burger.onclick = function() {
+burger.onclick = function () {
   this.classList.toggle("burger-off");
   this.classList.toggle("burger-on");
   document.querySelector(".dropdown").classList.toggle("dropdown-on");
